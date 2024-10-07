9bf76ee045a29d1f3d83762c9f6dffeefd42c7c2
Brandon Fitzwater <brandonfitzh2o@gmail.com>
2024-10-07 09:12:26 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  Button,
+  Provider as PaperProvider,
+  TextInput,
+} from "react-native-paper";
+import { useState } from "react";
 
 export default function App() {
+  const [email, setEmail] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <SafeAreaProvider
+        initialMetrics={{
+          frame: { x: 0, y: 0, width: 0, height: 0 },
+          insets: { top: 0, left: 0, right: 0, bottom: 0 },
+        }}
+      >
+        <View style={styles.container}>
+          <TextInput
+            autoComplete="email"
+            label="Email"
+            keyboardType="email-address"
+            maxLength={256}
+            mode="flat"
+            onBlur={() => Keyboard.dismiss()}
+            onChangeText={(email) => setEmail(email)}
+            right={
+              email.length > 0 ? (
+                <TextInput.Icon
+                  icon="close-circle-outline"
+                  onPress={() => setEmail("")}
+                />
+              ) : null
+            }
+            style={{ marginBottom: 16 }}
+            value={email}
+          />
+          <Button
+            disabled={email.trim().length === 0}
+            mode="contained"
+            onPress={() => console.log("ouch")}
+          >
+            Toggle Disabled
+          </Button>
+          <StatusBar style="auto" />
+        </View>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
+    justifyContent: "center",
   },
 });
